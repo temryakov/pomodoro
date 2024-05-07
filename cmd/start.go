a2f36ab655922c81a0df1f56e1b6684697a39b88
Maxim Temryakov <therevors@gmail.com>
2024-05-07 10:41:21 +0300

    Implemented simple start execution

--- cmd/start.go
+++ cmd/start.go
@@ -1,11 +1,12 @@
 /*
 Copyright © 2024 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
 	"fmt"
+	"pomodoro/pomodoro"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -13,7 +14,7 @@ import (
 // startCmd represents the start command
 var startCmd = &cobra.Command{
 	Use:   "start",
-	Short: "A brief description of your command",
+	Short: "Start new pomodoro",
 	Long: `A longer description that spans multiple lines and likely contains examples
 and usage of using your command. For example:
 
@@ -21,7 +22,14 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("start called")
+		timer := 1
+		duration := time.Duration(time.Second * 1)
+
+		fmt.Printf("pomodoro: 🍅 Pomodoro has been started! it will take %v minutes. Don't forget to take a break.\n", timer)
+		pomodoro.SetTimer(duration)
+
+		log.Println("🍅 Finished! Print 'pomodoro break' to take a break.")
+		pomodoro.Sound()
 	},
 }
 

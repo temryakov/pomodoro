05a3f46022cad1558cc38e2b1210144826905007
Maxim Temryakov <therevors@gmail.com>
2024-05-10 09:59:39 +0300

    Implemented interface for Finish method

--- domain/domain.go
+++ domain/domain.go
@@ -1,18 +1,9 @@
 package domain
 
-type Model struct {
-	Use               string
-	ShortDescription  string
-	LongDescription   string
-	StartDescription  string
-	FinishDescription string
-	Duration          int
-	HasFinish         bool
-	HasPause          bool
-}
-
 type IStatus interface {
-	Start(set int)
-	Finish(spent int)
+	Start(chan int)
+	Sound()
+	GetFinishDescription() string
+
 	// SelectOption() int
 }
---
+++ utils/finish.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"fmt"
+	"pomodoro/domain"
+)
+
+func Finish(status domain.IStatus) {
+	//TODO: Make "spent time.Duration" argument to pass spent time
+
+	fmt.Printf("\r\t⌛️ Total spent time: %v minutes\n", 25)
+	fmt.Println(status.GetFinishDescription())
+	status.Sound()
+}

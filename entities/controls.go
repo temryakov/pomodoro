13e363d3377b020540b910f3786a3ed1bb2a26e4
Maxim Temryakov <therevors@gmail.com>
2024-05-13 13:07:31 +0300

    Added resultbuilder

--- entities/controls.go
+++ entities/controls.go
@@ -3,6 +3,7 @@ package entities
 import (
 	"fmt"
 	"pomodoro/domain"
+	"pomodoro/utils"
 	"time"
 )
 
@@ -11,10 +12,11 @@ func Start(pb domain.PomodoroBreaker) {
 }
 
 func Finish(pb domain.PomodoroBreaker, spent time.Duration) {
-	m := int(spent.Minutes())
-	s := int(spent.Seconds()) - m*60
+	// m := int(spent.Minutes())
+	// s := int(spent.Seconds()) - m*60
 
-	fmt.Printf("\r\t⌛️ Total spent time: %2d minutes %2d seconds\n", m, s)
+	// fmt.Printf("\r\t⌛️ Total spent time: %2d minutes %2d seconds\n", m, s)
+	fmt.Print(utils.GetTimeSpentString(spent))
 	fmt.Println(pb.FinishDescription())
 	pb.Sound()
 }
---
+++ utils/resultbuilder.go
@@ -0,0 +1,24 @@
+package utils
+
+import (
+	"fmt"
+	"time"
+)
+
+func GetTimeSpentString(spent time.Duration) string {
+	var (
+		m      = int(spent.Minutes())
+		s      = int(spent.Seconds()) - m*60
+		header = "\r\t⌛️ Total spent time: "
+		ms     string
+		ss     string
+	)
+	if m > 0 {
+		ms = fmt.Sprintf("%2d minutes ", m)
+	}
+	if s > 0 {
+		ss = fmt.Sprintf("%2d seconds ", s)
+	}
+
+	return header + ms + ss + "\n"
+}

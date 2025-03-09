ca83571e6bdc083a05e9fdff7d776a7283dbb2ea
Maxim Temryakov <therevors@gmail.com>
2025-03-09 19:24:13 +0300

    style: left comments for getTimeRemaining calling

--- utils/timer.go
+++ utils/timer.go
@@ -28,6 +28,7 @@ func SetTimerWithContext(c context.Context, duration time.Duration) time.Duratio
 func SetTimer(ctx context.Context, duration time.Duration) {
 	remaining := time.Now().Add(duration)
 
+	// Retrieve the current remaining time, accounting for any potential delays in execution.
 	tr := getTimeRemaining(remaining)
 	fmt.Printf(constants.Countdown, tr.minutes, tr.seconds)
 

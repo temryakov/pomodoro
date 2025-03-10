5a92a50a31ae01392ec119584ec9db993b166e64
Maxim Temryakov <therevors@gmail.com>
2025-03-10 09:19:36 +0300

    fix: fix pause issue

--- utils/timer.go
+++ utils/timer.go
@@ -33,14 +33,20 @@ func SetTimerWithContext(duration time.Duration) time.Duration {
 }
 
 func (t *Timer) SetTimer() time.Duration {
+	// Is pause right now?
+	var pause bool
 
 	for range time.NewTicker(1 * time.Millisecond).C {
 		select {
 		case <-t.Pause:
-			<-t.Pause
+			pause = true
 		case <-t.Finish:
 			return t.Duration
 		default:
+			if pause {
+				<-t.Pause
+				pause = false
+			}
 			tr := getTimeRemaining(t.Duration)
 			if tr.total <= 0 {
 				return t.Duration

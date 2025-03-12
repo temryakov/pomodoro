a8a78e6b45a1c945de9d0ff1b3c0e6ad12377d01
Maxim Temryakov <therevors@gmail.com>
2025-03-12 09:41:40 +0300

    feat: implement db init

---
+++ utils/db.go
@@ -0,0 +1,54 @@
+package utils
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func InitDB() *sql.DB {
+	path, err := buildPath()
+	if err != nil {
+		panic(err)
+	}
+	db, err := sql.Open("sqlite3", path)
+
+	if err != nil {
+		panic(err)
+	}
+
+	_, err = createTable(db)
+
+	if err != nil {
+		fmt.Println("CREATE TABLE ERROR")
+		panic(err)
+	}
+	return db
+}
+
+func buildPath() (string, error) {
+	dirname, err := os.UserHomeDir()
+	if err != nil {
+		panic(err)
+	}
+	dirname += "/.pomodoro"
+
+	if _, err := os.Stat(dirname); os.IsNotExist(err) {
+		err := os.Mkdir(dirname, os.ModePerm)
+		if err != nil {
+			panic(err)
+		}
+	}
+	return fmt.Sprintf("%s/.pomodoro/store.db", dirname), nil
+}
+
+func createTable(db *sql.DB) (sql.Result, error) {
+	sql := `CREATE TABLE IF NOT EXISTS Pomodoro (
+        name     TEXT NOT NULL,
+        duration     TEXT NOT NULL
+    );`
+
+	return db.Exec(sql)
+}
